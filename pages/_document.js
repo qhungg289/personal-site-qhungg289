1fa95e6d482a8124632486a0493e82e4d207dcda
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-15 21:12:13 +0700

    Move font to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html>
-			<Head />
-			<body className="bg-slate-50 font-inter">
+			<Head>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body className="min-h-screen bg-slate-50 font-inter">
 				<Main />
 				<NextScript />
 			</body>
