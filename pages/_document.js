eda2fd75615cd8e33d87bc6404d00f2f13943118
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-16 09:17:39 +0700

    Adjust default height for document

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html>
+		<Html className="h-full">
 			<Head>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
-			<body className="min-h-screen bg-slate-50 font-inter">
+			<body className="h-full bg-slate-50 font-inter">
 				<Main />
 				<NextScript />
 			</body>
