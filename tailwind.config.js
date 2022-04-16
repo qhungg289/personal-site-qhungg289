f3dbbabe964f99cde9eb46a3ed9eecf4113f5055
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-16 16:14:30 +0700

    Add bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				hero: "url('../public/img/bg-image.jpg')",
+			},
+		},
 		fontFamily: {
 			inter: ["Inter", "sans-serif"],
 		},
