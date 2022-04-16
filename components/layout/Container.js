130e973f5ffc29ba102798fcc57f7d5687f2aa2b
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-16 20:26:20 +0700

    Change styling of Container

--- components/layout/Container.js
+++ components/layout/Container.js
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-	return <div className="container h-full overflow-auto px-4">{children}</div>;
+	return <div className="container mt-20 px-4">{children}</div>;
 }
