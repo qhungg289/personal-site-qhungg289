434c8c33711ae6b40f15564922357dcf5ae8c0b3
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-16 20:00:29 +0700

    Change z-index values

--- components/layout/BackgroundImage.js
+++ components/layout/BackgroundImage.js
@@ -1,8 +1,8 @@
 export default function BackgroundImage() {
 	return (
 		<>
-			<div className="absolute inset-0 -z-10 bg-hero bg-cover bg-center"></div>
-			<div className="absolute inset-0 -z-[5] bg-gradient-to-b from-transparent to-slate-50"></div>
+			<div className="absolute inset-0 -z-20 bg-hero bg-cover bg-center"></div>
+			<div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent to-slate-50"></div>
 		</>
 	);
 }
