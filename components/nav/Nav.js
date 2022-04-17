52dd977c7a309bab1e4e944bbc66a6a19b06d62f
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-17 10:57:18 +0700

    Use container in Nav

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -3,32 +3,34 @@ import Link from "next/link";
 
 export default function Nav() {
 	return (
-		<nav className="fixed top-0 z-10 flex h-20 w-full items-center justify-between border-b-2 bg-white/50 p-4 backdrop-blur-md">
-			<div className="space-x-5 sm:space-x-9">
-				<NavLink href="/" pathName="Home" />
-				<NavLink href="/about" pathName="About" />
-				<NavLink href="/contacts" pathName="Contacts" />
-			</div>
+		<nav className="fixed top-0 z-10  h-20 w-full  border-b-2 bg-white/50 p-4 backdrop-blur-md">
+			<div className="container flex items-center justify-between">
+				<div className="space-x-5 sm:space-x-14">
+					<NavLink href="/" pathName="Home" />
+					<NavLink href="/about" pathName="About" />
+					<NavLink href="/contacts" pathName="Contacts" />
+				</div>
 
-			<div>
-				<Link href={"/portfolio"} passHref>
-					<a className="flex items-center gap-2 rounded-md border-2 border-blue-600 bg-blue-600 px-3 py-2 text-white transition-all hover:bg-transparent hover:text-blue-600">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-5 w-5"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-							<path
-								fillRule="evenodd"
-								d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-								clipRule="evenodd"
-							/>
-						</svg>
-						Portfolio
-					</a>
-				</Link>
+				<div>
+					<Link href={"/portfolio"} passHref>
+						<a className="flex items-center gap-2 rounded-md border-2 border-blue-600 bg-blue-600 px-3 py-2 text-white transition-all hover:bg-transparent hover:text-blue-600">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+								<path
+									fillRule="evenodd"
+									d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							Portfolio
+						</a>
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);
