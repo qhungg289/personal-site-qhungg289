cc47db360618b1dd3ad3445499f940f0c7a2e775
Hung Duong Quang <qhungg289@protonmail.com>
2022-04-22 20:49:03 +0700

    Increase gap in GridCardContainer

--- components/layout/GridCardContainer.js
+++ components/layout/GridCardContainer.js
@@ -1,6 +1,6 @@
 export default function GridCardContainer({ children }) {
 	return (
-		<div className="mb-8 grid gap-y-2 md:grid-cols-3 md:gap-x-2">
+		<div className="mb-8 grid gap-y-3 md:grid-cols-3 md:gap-x-3">
 			{children}
 		</div>
 	);
