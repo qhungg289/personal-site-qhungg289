f01c831e167777149407d0d9b1a87c5efb7651af
Hung Duong Quang <qhungg289@protonmail.com>
2022-05-29 16:26:02 +0700

    Update links styling

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -23,7 +23,7 @@ export default function Contacts() {
 				<ul className="mt-4 list-inside list-disc">
 					<li>
 						Phone number:{" "}
-						<a href="tel:0853866630" className="hover:underline">
+						<a href="tel:0853866630" className="text-blue-600 hover:underline">
 							0853866630
 						</a>
 					</li>
@@ -31,27 +31,30 @@ export default function Contacts() {
 						Email:{" "}
 						<a
 							href="mailto:duongquanghung749@gmail.com"
-							className="hover:underline"
+							className="text-blue-600 hover:underline"
 						>
 							duongquanghung749@gmail.com
 						</a>
 					</li>
 					<li>
-						<a href="https://github.com/qhungg289" className="hover:underline">
+						<a
+							href="https://github.com/qhungg289"
+							className="text-blue-600 hover:underline"
+						>
 							GitHub
 						</a>
 					</li>
 					<li>
 						<a
 							href="https://facebook.com/qhungg289"
-							className="hover:underline"
+							className="text-blue-600 hover:underline"
 						>
 							Facebook
 						</a>
 					</li>
 					<li>
 						<a
-							className="hover:underline"
+							className="text-blue-600 hover:underline"
 							href="https://www.twitter.com/qhungg289"
 						>
 							Twitter
