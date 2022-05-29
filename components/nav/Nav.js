0b3ac84d57a8af2cbfdd1f86130dd90130266903
Hung Duong Quang <qhungg289@protonmail.com>
2022-05-29 16:42:58 +0700

    Remove Darkmode button

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -62,23 +62,6 @@ export default function Nav() {
 					<NavLink href="/portfolio" pathName="Portfolio" />
 					<NavLink href="/contacts" pathName="Contacts" />
 				</div>
-
-				<button className="rounded-md border-2 p-1 transition-colors hover:border-slate-400">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-						/>
-					</svg>
-				</button>
 			</div>
 		</nav>
 	);
