385f450b5b493a6f0bc62a2e3816489051ec40ae
Hung Duong Quang <qhungg289@protonmail.com>
2022-05-29 16:09:29 +0700

    Add content for Contacts route

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -17,6 +17,47 @@ export default function Contacts() {
 
 			<Container>
 				<MainHeader>Contacts</MainHeader>
+
+				<h2 className="text-2xl font-medium">Get in touch with me</h2>
+
+				<ul className="mt-4 list-inside list-disc">
+					<li>
+						Phone number:{" "}
+						<a href="tel:0853866630" className="hover:underline">
+							0853866630
+						</a>
+					</li>
+					<li>
+						Email:{" "}
+						<a
+							href="mailto:duongquanghung749@gmail.com"
+							className="hover:underline"
+						>
+							duongquanghung749@gmail.com
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/qhungg289" className="hover:underline">
+							GitHub
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://facebook.com/qhungg289"
+							className="hover:underline"
+						>
+							Facebook
+						</a>
+					</li>
+					<li>
+						<a
+							className="hover:underline"
+							href="https://www.twitter.com/qhungg289"
+						>
+							Twitter
+						</a>
+					</li>
+				</ul>
 			</Container>
 
 			<Footer />
