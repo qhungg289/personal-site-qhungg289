9ffa8b2d1c00f417047dfd23c4e3565e4db0b39e
Hung Duong Quang <qhungg289@protonmail.com>
2022-06-11 21:54:28 +0700

    Remove unused font and change text color

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,11 @@ export default function Document() {
 		<Html>
 			<Head>
 				<link
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-					rel="stylesheet"
-				/>
-				<link
 					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
-			<body className="h-full bg-slate-100 font-ibm-plex-sans text-neutral-800">
+			<body className="relative bg-slate-100 font-ibm-plex-sans text-slate-800">
 				<Main />
 				<NextScript />
 			</body>
