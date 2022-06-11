d5d4f11c93237725b3ebdf043d19b0f4d52f728d
Hung Duong Quang <qhungg289@protonmail.com>
2022-06-11 21:21:11 +0700

    Add custom animation keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				breath: {
+					"0%, 100%": { opacity: 1 },
+					"50%": { opacity: 0 },
+				},
+			},
+			animation: {
+				breath: "breath 1s ease-in-out infinite",
+			},
+		},
 		fontFamily: {
 			inter: ["Inter", "sans-serif"],
 			"ibm-plex-sans": ["IBM Plex Sans", "sans-serif"],
