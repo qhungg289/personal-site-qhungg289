ff13b2ccb98fd937852e691887ae249337555748
Hung Duong Quang <qhungg289@protonmail.com>
2022-06-12 19:33:14 +0700

    Use IBM Plex Mono font

--- components/typography/MainHeader.js
+++ components/typography/MainHeader.js
@@ -1,3 +1,7 @@
 export default function MainHeader({ children }) {
-	return <h1 className="mb-4 text-4xl font-bold md:text-5xl">{children}</h1>;
+	return (
+		<h1 className="mb-4 font-ibm-plex-mono text-4xl font-bold md:text-5xl">
+			{children}
+		</h1>
+	);
 }
