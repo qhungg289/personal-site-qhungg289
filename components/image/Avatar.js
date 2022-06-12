59c8b813afc6ce9e834cd11c24d5a275e008da37
Hung Duong Quang <qhungg289@protonmail.com>
2022-06-12 21:55:08 +0700

    Change Avatar width

--- components/image/Avatar.js
+++ components/image/Avatar.js
@@ -3,7 +3,7 @@ import avatar from "../../public/img/avatar.jpg";
 
 export default function Avatar() {
 	return (
-		<div className="absolute inset-y-0 right-0 w-60 md:w-72 lg:w-80">
+		<div className="absolute inset-y-0 right-0 -z-10 w-44 md:w-72 lg:w-80">
 			<Image src={avatar} alt="Avatar" className="rounded-bl-full" />
 		</div>
 	);
